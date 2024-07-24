b1ca2b684b772dcec9891dec41f54967eb9720d9
David Awoyelu <awoyelu123@gmail.com>
2024-07-24 16:39:14 +0100

    Added code that will allow me to connect to the database,read the loan_payments table, create a data frame of this table and save the file locally as a csv file

--- db_utils.py
+++ db_utils.py
@@ -1,2 +1,38 @@
+import yaml
+from sqlalchemy import create_engine
+import pandas as pd
+from pathlib import Path  
+
 class RDSDatabaseConnector:
-    pass
+
+    def __init__(self,):
+        self.credentials = {}
+
+    def load_credentials(self):
+        with open('credentials.yaml', 'r') as file:
+            self.credentials = yaml.safe_load(file)
+        return(self.credentials)
+
+    def connect_to_database(self):
+        engine = create_engine(f"{self.credentials['DATABASE_TYPE']}+{self.credentials['DBAPI']}://{self.credentials['USER']}:{self.credentials['PASSWORD']}@{self.credentials['HOST']}:{self.credentials['PORT']}/{self.credentials['DATABASE']}")
+        engine.execution_options(isolation_level='AUTOCOMMIT').connect()
+        engine.connect()
+        self.data = pd.read_sql_table('loan_payments',con = engine)
+        print("Connection Successful")
+
+
+    def create_dataframe(self):
+        self.df = pd.DataFrame(self.data)
+
+    def save_data_as_csv(self):
+        filepath = Path("C:\\Users\\awoye\OneDrive\\Documents\\GitHub\\finance_eda\\data.csv'")  
+        filepath.parent.mkdir(parents=True, exist_ok=True)  
+        self.df.to_csv(filepath) 
+
+    def shape(self):
+        local_data = pd.read_csv("C:\\Users\\awoye\OneDrive\\Documents\\GitHub\\finance_eda\\data.csv'")
+        local_df = pd.DataFrame(local_data)
+
+p = RDSDatabaseConnector()
+p.shape()
+
